7e893051ad62097e95cd001cfa9d7cf12590fe4c
EC2 Default User <ec2-user@ip-172-31-37-183.ec2.internal>
2022-06-07 13:40:11 +0000

    INitial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,12 @@
 class BooksController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def show
-    @book = Book.find(params[:id])
-    @user = @book.user
+    @book = Book.new
+    @detail = Book.find(params[:id])
+    @user = @detail.user
+    @books = Book.all
+    @post_comment = PostComment.new
   end
 
   def index
@@ -44,5 +48,12 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def ensure_correct_user
+   @book = Book.find(params[:id])
+    unless @book.user == current_user
+        redirect_to books_path
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,11 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
   has_one_attached :profile_image
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   validates :name, presence: true, length: { minimum: 2, maximum: 20 }, uniqueness: true
-  validates :introduction, presence: true, length: {maximum: 50}
+  validates :introduction, length: {maximum: 50}
 
 
   def get_profile_image
